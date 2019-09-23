708b22bc2567ac3f3eea3679cb447370603be377
Christina King <kingcm41@hotmail.com>
2019-09-22 21:07:43 -0400

    score and skill update
    increased skill difficulty and made corrected final score printing erroneous

--- inc/complete.php
+++ inc/complete.php
@@ -13,21 +13,23 @@ $selection9 = htmlspecialchars($_SESSION['selection'][9]);
 $selection10 = htmlspecialchars($_SESSION['selection'][10]);
 
 
-if(isset($_SESSION['selection'][10])){
+if($_SESSION['score'] >= 1){
 
-echo "<h1> Final Score: " . ++$_SESSION['score'] . " out of 10</h1>";
+echo "<h1> Final Score: " . $_SESSION['score'] . " out of 10</h1>";}
 
-if($_SESSION['score']>= 7){
-
-echo "<h2> Great Job, You're a Math SuperStar!! </h2>";
-
-} else{
-
+elseif($_SESSION['score'] == NULL){
+echo "<h1> Final Score: " . 0 . " out of 10</h1>";
 echo "<h2> Keep Practicing! </h2>";
 
 }
+if($_SESSION['score']>= 7){
 
+      echo "<h2> Great Job, You're a Math SuperStar!! </h2>";
+
+}elseif($_SESSION['score'] !== NULL AND $_SESSION['score'] < 7) {
+       echo "<h2> Keep Practicing! </h2>";
+}
+session_destroy();
 echo "<form action='/index.php' method='post'>";
 echo "<input type='submit' class='btn' name='Retest' value='Play Again'>";
 echo "</form>";
-}
--- inc/quiz.php
+++ inc/quiz.php
@@ -2,11 +2,9 @@
 session_start();
 // quiz session creator
 include 'inc/question_set.php';
-
 $total = 10;
 $question = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_NUMBER_INT);
 if (empty ($question)){
-
   session_destroy();
   $score = 0;
   $question = 1;
@@ -14,41 +12,37 @@ if (empty ($question)){
 //tracks answers upon submission
 if(isset($_POST['selection'])){
    $_SESSION['selection'][$question-1] = filter_input(INPUT_POST, 'selection', FILTER_SANITIZE_STRING);
-
-
 }
 if (isset($_POST['correct'])) {
         $_SESSION['correct'] = filter_input(INPUT_POST, 'correct', FILTER_SANITIZE_NUMBER_INT);
-
 }
-
-if($question > $total){
-
+/*if($question > $total){
    header('location: inc/complete.php');
    exit;
-
-}
-
+}*/
 //site interaction code
-shuffle($answer_choice);
+shuffle($answer_choice2);
 echo "<p class='breadcrumbs'>Question " . $question . " of " . $total . "</p>";
-
 echo '<form method = "post" action="index.php?p='. ($question+1) . '" />';
 //echo "<input type='hidden' name='begin' value='0' />";
-echo "<p class='quiz'> Solve " . $a . "+" . $b . "= </p>";
-echo "<input type='submit' class='btn' name='selection' value=' " . $answer_choice[0] . "'>";
-echo "<input type='submit' class='btn' name='selection' value=' " . $answer_choice[1] . "'>";
-echo "<input type='submit' class='btn' name='selection' value=' " . $answer_choice[2] . "'>";
-echo "<input type='hidden' name='correct' value='" . $correct_answer . "'>";
+echo "<p class='quiz'> Solve " . $e . "+" . $f . "= </p>";
+echo "<input type='submit' class='btn' name='selection' value=' " . $answer_choice2[0] . "'>";
+echo "<input type='submit' class='btn' name='selection' value=' " . $answer_choice2[1] . "'>";
+echo "<input type='submit' class='btn' name='selection' value=' " . $answer_choice2[2] . "'>";
+echo "<input type='hidden' name='correct' value='" . $correct_answer2 . "'>";
 //answer validation with toast
   if ($_SESSION['selection'][$question-1] == $_SESSION['correct'] AND isset($_POST['selection'])){
   $toast = ["Right!", "You Got It!", "Keep It Up!"];
   shuffle($toast);
   echo "<p class='breadcrumbs'>" . implode(array_slice($toast, 2)) . "</p>";
   ++$_SESSION['score'];
-
 } elseif ($_SESSION['selection'][$question-1] != $_SESSION['correct'] AND isset($_POST['selection'])){
   $toastIncorrect = ["Darn!", "Maybe Next Time!", "Sorry!"];
   shuffle($toastIncorrect);
   echo "<p class='breadcrumbs'>" . implode(array_slice($toastIncorrect, 2)) . "</p>";
+  
    }
+   if(isset($_SESSION['selection'][10])){
+      header('location: inc/complete.php');
+      exit;
+    }
